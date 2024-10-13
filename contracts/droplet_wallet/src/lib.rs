7d520d91ed9b463448f8540b94f1a4f049fc2d6a
sanfarans <jerzkra@gmail.com>
2024-10-13 04:11:33 +0200

    feat: add charity micro-donations

--- contracts/droplet_wallet/src/lib.rs
+++ contracts/droplet_wallet/src/lib.rs
@@ -1,15 +1,26 @@
 #![no_std]
 
+use core::panic;
+
 use soroban_sdk::token::TokenClient;
 use soroban_sdk::{contract, contractimpl, symbol_short, Address, Env, Symbol};
 
 const OWNER: Symbol = symbol_short!("OWNER");
+const CHARITY_ADDRESS: Symbol = symbol_short!("CHAR_ADDR");
+const CHARITY_FEE: Symbol = symbol_short!("CHAR_PERC");
 
 #[contract]
 pub struct DropletWallet;
 
 #[contractimpl]
 impl DropletWallet {
+    /// Initializes the contract with the given owner address.
+    /// This function should be called only once.
+    ///
+    /// # Arguments
+    ///
+    /// * `env` - The environment interface for interacting with the blockchain.
+    /// * `owner` - The address of the owner who can authorize actions on the contract.
     pub fn init(env: Env, owner: Address) {
         if (env.storage().instance().get::<Symbol, Address>(&OWNER)).is_some() {
             panic!("Already initialized");
@@ -19,6 +30,15 @@ impl DropletWallet {
         env.storage().instance().extend_ttl(max_ttl, max_ttl);
     }
 
+    /// Authenticates that the caller is the owner.
+    ///
+    /// # Arguments
+    ///
+    /// * `env` - The environment interface.
+    ///
+    /// # Returns
+    ///
+    /// Returns the owner's address if authenticated.
     fn auth_owner(env: &Env) -> Address {
         if !(env.storage().instance().get::<Symbol, Address>(&OWNER)).is_some() {
             panic!("Not initialized")
@@ -30,6 +50,13 @@ impl DropletWallet {
         owner
     }
 
+    /// Funds the contract with the specified amount of tokens from the owner.
+    ///
+    /// # Arguments
+    ///
+    /// * `env` - The environment interface.
+    /// * `token_address` - The address of the token to fund.
+    /// * `amount` - The amount of tokens to transfer to the contract.
     pub fn fund(env: Env, token_address: Address, amount: i128) {
         let owner = Self::auth_owner(&env);
         let token_client = TokenClient::new(&env, &token_address);
@@ -37,6 +64,13 @@ impl DropletWallet {
         token_client.transfer(&owner, &to, &amount);
     }
 
+    /// Withdraws the specified amount of tokens from the contract to the owner.
+    ///
+    /// # Arguments
+    ///
+    /// * `env` - The environment interface.
+    /// * `token_address` - The address of the token to withdraw.
+    /// * `amount` - The amount of tokens to transfer to the owner.
     pub fn withdraw(env: Env, token_address: Address, amount: i128) {
         let owner = Self::auth_owner(&env);
         let token_client = TokenClient::new(&env, &token_address);
@@ -44,11 +78,69 @@ impl DropletWallet {
         token_client.transfer(&from, &owner, &amount);
     }
 
+    /// Transfers the specified amount of tokens from the contract to the given address.
+    /// If charity address exists and donation fee is positive, a micro-donation is subtracted
+    /// from the amount and transferred to charity of choice.
+    ///
+    /// # Arguments
+    ///
+    /// * `env` - The environment interface.
+    /// * `token_address` - The address of the token to transfer.
+    /// * `to` - The address to transfer tokens to.
+    /// * `amount` - The amount of tokens to transfer.
     pub fn transfer(env: Env, token_address: Address, to: Address, amount: i128) {
         Self::auth_owner(&env);
         let token_client = TokenClient::new(&env, &token_address);
         let from = env.current_contract_address();
+
+        let fee: Option<i128> = env.storage().instance().get(&CHARITY_FEE);
+        let charity_address: Option<Address> = env.storage().instance().get(&CHARITY_ADDRESS);
+
+        if let (Some(fee), Some(charity_address)) = (fee, charity_address) {
+            if fee > 0 {
+                let fee = amount * fee / 10000;
+
+                if fee > amount {
+                    panic!("Amount is less than the calculated fee");
+                }
+
+                let amount_after_fee = amount - fee;
+
+                token_client.transfer(&from, &charity_address, &fee);
+                token_client.transfer(&from, &to, &amount_after_fee);
+
+                return;
+            }
+        }
+
+        // If no charity setup or fee is zero, transfer full amount
         token_client.transfer(&from, &to, &amount);
+    }
+
+    /// Sets up the charity address and the fee of tokens to donate.
+    ///
+    /// The fee should be provided in basis points (bips), where 1 bip = 0.01%.
+    /// For example, to set a fee of 1%, use 100 bips.
+    ///
+    /// # Arguments
+    ///
+    /// * `env` - The environment interface.
+    /// * `charity_address` - The address of the charity to donate to.
+    /// * `fee` - The percentage (in bips) of tokens to donate.
+    pub fn setup_charity(env: Env, charity_address: Address, fee: i128) {
+        Self::auth_owner(&env);
+
+        if fee > 10000 {
+            panic!("Fee cannot be more than 10000 bips (100%)");
+        }
+        if fee < 0 {
+            panic!("Fee cannot be lower than 0.")
+        }
+
+        env.storage()
+            .instance()
+            .set(&CHARITY_ADDRESS, &charity_address);
+        env.storage().instance().set(&CHARITY_FEE, &fee);
     }
 }
 
--- contracts/droplet_wallet/src/test.rs
+++ contracts/droplet_wallet/src/test.rs
@@ -25,16 +25,16 @@ fn test_fund_and_withdrawal() {
     smart_wallet_client.init(&alice);
 
     let token_admin = Address::generate(&env);
-    let (token_a, token_a_admin) = create_token_contract(&env, &token_admin);
-    token_a_admin.mint(&alice, &1000);
+    let (token, token_admin) = create_token_contract(&env, &token_admin);
+    token_admin.mint(&alice, &1000);
 
-    smart_wallet_client.fund(&token_a.address, &1000);
-    assert_eq!(token_a.balance(&smart_wallet_id), 1000);
-    assert_eq!(token_a.balance(&alice), 0);
+    smart_wallet_client.fund(&token.address, &1000);
+    assert_eq!(token.balance(&smart_wallet_id), 1000);
+    assert_eq!(token.balance(&alice), 0);
 
-    smart_wallet_client.withdraw(&token_a.address, &1000);
-    assert_eq!(token_a.balance(&smart_wallet_id), 0);
-    assert_eq!(token_a.balance(&alice), 1000);
+    smart_wallet_client.withdraw(&token.address, &1000);
+    assert_eq!(token.balance(&smart_wallet_id), 0);
+    assert_eq!(token.balance(&alice), 1000);
 }
 
 #[test]
@@ -49,25 +49,68 @@ fn test_transfer() {
     let bob = Address::generate(&env);
 
     let token_admin = Address::generate(&env);
-    let (token_a, token_a_admin) = create_token_contract(&env, &token_admin);
-    token_a_admin.mint(&alice, &1000);
+    let (token, token_admin) = create_token_contract(&env, &token_admin);
+    token_admin.mint(&alice, &1000);
 
-    assert_eq!(token_a.balance(&alice), 1000);
+    assert_eq!(token.balance(&alice), 1000);
 
     smart_wallet_client.init(&alice);
     let initial_wallet_balance = 500;
-    smart_wallet_client.fund(&token_a.address, &initial_wallet_balance);
+    smart_wallet_client.fund(&token.address, &initial_wallet_balance);
 
-    assert_eq!(token_a.balance(&smart_wallet_id), 500);
-    assert_eq!(token_a.balance(&alice), 500);
-    assert_eq!(token_a.balance(&bob), 0);
+    assert_eq!(token.balance(&smart_wallet_id), 500);
+    assert_eq!(token.balance(&alice), 500);
+    assert_eq!(token.balance(&bob), 0);
 
     let transfer_amount = 200;
-    smart_wallet_client.transfer(&token_a.address, &bob, &transfer_amount);
+    smart_wallet_client.transfer(&token.address, &bob, &transfer_amount);
 
-    let to_balance = token_a.balance(&bob);
-    assert_eq!(to_balance, transfer_amount);
+    assert_eq!(token.balance(&bob), transfer_amount);
 
-    let wallet_balance = token_a.balance(&smart_wallet_client.address);
-    assert_eq!(wallet_balance, initial_wallet_balance - transfer_amount);
+    assert_eq!(
+        token.balance(&smart_wallet_client.address),
+        initial_wallet_balance - transfer_amount
+    );
+}
+
+#[test]
+fn test_transfer_with_charity_donation() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let smart_wallet_id = env.register_contract(None, DropletWallet);
+    let smart_wallet_client = DropletWalletClient::new(&env, &smart_wallet_id);
+
+    let alice = Address::generate(&env);
+    let bob = Address::generate(&env);
+
+    let token_admin = Address::generate(&env);
+    let (token, token_admin) = create_token_contract(&env, &token_admin);
+    token_admin.mint(&alice, &1000);
+
+    assert_eq!(token.balance(&alice), 1000);
+
+    smart_wallet_client.init(&alice);
+    let charity = Address::generate(&env);
+    let fee = 100; // 1%
+    smart_wallet_client.setup_charity(&charity, &fee);
+
+    let initial_wallet_balance = 1000;
+    smart_wallet_client.fund(&token.address, &initial_wallet_balance);
+
+    assert_eq!(token.balance(&smart_wallet_id), 1000);
+    assert_eq!(token.balance(&alice), 0);
+    assert_eq!(token.balance(&bob), 0);
+    assert_eq!(token.balance(&charity), 0);
+
+    let transfer_amount = 800;
+    let expected_fee = 8;
+    smart_wallet_client.transfer(&token.address, &bob, &transfer_amount);
+    assert_eq!(
+        token.balance(&smart_wallet_client.address),
+        initial_wallet_balance - transfer_amount
+    );
+
+    assert_eq!(token.balance(&bob), transfer_amount - expected_fee);
+    assert_eq!(token.balance(&charity), expected_fee);
 }
